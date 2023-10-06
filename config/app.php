3125bc30172954e6fbca3f3d5e0aa499378aaf9f
ramasakti <ramasakti1337@gmail.com>
2023-10-06 15:10:42 +0700

    Salah /config/app.php

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\NavServiceProvider::class,
         Laravel\Sanctum\SanctumServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         // Maatwebsite\Excel\ExcelServiceProvider::class,
